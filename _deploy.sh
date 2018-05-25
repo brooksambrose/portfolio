9f623f02016f5569362064b59c15ceef9d89c452
Brooks Ambrose <brooksambrose@gmail.com>
2018-05-25 15:21:54 -0700

    for circleci

--- _deploy.sh
+++ _deploy.sh
@@ -7,22 +7,15 @@
 # make sure you're on the master branch
 git checkout master
 
-cd /home/rstudio/ \
-&& rm -rf docs/ \
+# assuming you've already built it, replace old docs with new _books build
+rm -rf docs/ \
 && mv -u -T _book/ docs/
 
-# assuming you've already built it, replace old docs with new _books build
+# site will be public so dissuade robots and search engines from crawling your page
 echo "User-agent: *
 Disallow: /" > docs/robots.txt
 
-# site will be public so dissuade robots and search engines from crawling your page
+# push
 git add . \
 && git commit -m "deploy" || true \
-&& echo '
-Ready to push with authorization.
-In Rstudio use menu "Tools > Shell..." and enter:
-
-git push origin master
-	
-Or from an open Rstudio project use menu
-"Git > Push Branch" or use tab "Git > Push".'
+&& echo 'Ready to push.'
