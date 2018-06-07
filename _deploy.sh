92ff7177ba00bbdf7ba85c1ef620fe6666b3f350
Default user <brooksambrose@gmail.com>
2018-06-07 18:04:01 +0000

    fixup!

--- _deploy.sh
+++ _deploy.sh
@@ -8,6 +8,6 @@
 # commit
 git checkout master \
 && echo -e "User-agent: *\nDisallow: /" > _book/robots.txt \
-&& git add _book
+&& git add _book \
 && git -c user.email="brooksambrose-machine@gmail.com" commit --allow-empty -m "deploy $(date +'%Y-%m-%d %H:%M:%S')" || true \
 && git subtree push --prefix _book origin gh-pages
