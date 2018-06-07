d07af9f582245d0f0f66eefc5db8404d587fb537
Default user <brooksambrose@gmail.com>
2018-06-07 18:20:11 +0000

    worktree deploy

--- _build.sh
+++ _build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rm -rf _book/ _main*md \
+&& git worktree add _book gh-pages \
 && R -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook")'
--- _deploy.sh
+++ _deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-# make sure you're on the gh-pages branch
-# delete everything (replace old site) and copy _book from master
-# copy book directory to gh-pages
-# empty _book into parent, delete _book
+# make sure you're on the master branch
 # site will be public so dissuade robots and search engines from crawling your page
+# add _book as worktree of gh-pages
+# descend into _book and add changes
 # commit
+# force push worktree
+# return to start
 git checkout master \
 && echo -e "User-agent: *\nDisallow: /" > _book/robots.txt \
-&& git add _book \
-&& git -c user.email="brooksambrose-machine@gmail.com" commit --allow-empty -m "deploy $(date +'%Y-%m-%d %H:%M:%S')" || true \
-&& git subtree push --prefix _book origin gh-pages
+&& cd _book && git add . && git status \
+&& git -c user.email="brooksambrose-machine@gmail.com" commit --allow-empty -m "deploy $(git log '--format=format:%h' master -1)" \
+&& git push origin gh-pages --force \
+&& cd ..
