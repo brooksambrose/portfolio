f1645b1420e909418065fbde334a42e5aaff6ce0
Default user <brooksambrose@gmail.com>
2018-06-11 22:20:42 +0000

    fixup! added fetch

--- _save.sh
+++ _save.sh
@@ -1,7 +1,7 @@
 # CHECK
 # resources currently being ignored
 git status -s && git check-ignore -v * && git add $(read -er -p "Need to add anything?: " UI && echo $UI) \
-&& git commit -m"w$(date +'%V %a %p') $(read -r -p 'Explain this commit: ' UI && echo $UI)"
+&& git commit -m"w$(date +'%V %a %p') $(read -r -p 'Explain this commit: ' UI && echo $UI)" && git fetch
 read -n1 -p "Ready to rebase?" doit 
 case $doit in  
   y|Y) git rebase -i $(log -1 --reverse --pretty=format:"%h" origin/draft..draft) ;; 
