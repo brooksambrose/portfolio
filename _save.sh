56440c916dfe0f675a3d8ecd908c8dcb7193684f
Default user <brooksambrose@gmail.com>
2018-06-11 22:14:29 +0000

    w24 Mon PM more informative commit message, case handling

--- _save.sh
+++ _save.sh
@@ -1,5 +1,8 @@
 # CHECK
 # resources currently being ignored
 git status -s && git check-ignore -v * && git add $(read -er -p "Need to add anything?: " UI && echo $UI) \
-&& git commit -m"w$(date +%V) $(read -r -p 'Explain this commit: ' UI && echo $UI)" \
-&& git rebase -i $(log -1 --reverse --pretty=format:"%h" origin/draft..draft)
+&& git commit -m"w$(date +'%V %a %p') $(read -r -p 'Explain this commit: ' UI && echo $UI)"
+read -n1 -p "Ready to rebase?" doit 
+case $doit in  
+  y|Y) git rebase -i $(log -1 --reverse --pretty=format:"%h" origin/draft..draft) ;; 
+esac
