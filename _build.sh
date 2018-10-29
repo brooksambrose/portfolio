d88d9e214e00e9b3ef1168beecf655e8d1181741
Brooks Ambrose <brooksambrose@gmail.com>
2018-10-29 19:55:09 +0000

    update discipline graph

--- _build.sh
+++ _build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git worktree add _book gh-pages || true \
-&& rm -rf _book/* _bookdown_files _main*md \
+&& rm -rf .RData ._book/* _bookdown_files _main*md \
 && R -e "rmarkdown::render_site(encoding = 'UTF-8')"
 
 # command to automatically detect branch and add to edit link
