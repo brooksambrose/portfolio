852cc74cc5ab3225a43d751c2cd4e4e7ca56cdd0
Brooks Ambrose <brooksambrose@gmail.com>
2018-12-26 21:48:49 +0000

    xmas checkpoint

--- _build.sh
+++ _build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git worktree add _book gh-pages || true \
 && rm -rf .RData ._book/* _bookdown_files _main*md \
-&& R -e "rmarkdown::render_site(encoding = 'UTF-8')"
+&& R -e "rmarkdown::render_site(output_format='bookdown::gitbook',encoding = 'UTF-8')"
 
 # command to automatically detect branch and add to edit link
 # $(git branch | grep * | cut -d ' ' -f2)
