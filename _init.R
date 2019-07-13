8ac7fa5dad93d24e02b15d22bdd55842230373a9
Default user <brooksambrose@gmail.com>
2019-07-13 04:22:40 +0000

    fixup

--- _init.R
+++ _init.R
@@ -23,15 +23,17 @@ ini<-'https://api.zotero.org/users/2730456/collections/SAY3BJDZ/items?format=bib
 gt<-list()
 gt[[1]]<-try(httr::GET(ini),silent = T)
 while(inherits(gt[[1]],'try-error')) gt[[1]]<-try(httr::GET(ini),silent = T)
-nxtf<-function(x) {{httr::headers(x)$link %>% httr::parse_media(.)}$complete %>% gsub('(^<)|(>$)','',.) %>% paste0('&key=9ekyrSk1IIye3OH3Yh2l7ftJ') %>% {r<-sub('(.+collections/)([^/]+)(.+)','\\2',.);sub('(.+collections/)([^/]+)(.+)',paste0('\\1',toupper(r),'\\3'),.)}}
+nxtf<-function(x) {{httr::headers(gt[[length(gt)]])$link} %>% strsplit(split=',') %>%  unlist %>% grep('next',.,value = T) %>% sub('.*<([^>]+).+','\\1',.) %>% paste0('&key=9ekyrSk1IIye3OH3Yh2l7ftJ') %>% {r<-sub('(.+collections/)([^/]+)(.+)','\\2',.);sub('(.+collections/)([^/]+)(.+)',paste0('\\1',toupper(r),'\\3'),.)}}
 nxt<-nxtf(gt[[1]])
 con<-list()
 con[[1]]<-httr::content(gt[[1]],type='text',encoding = 'UTF-8')
-while({httr::headers(gt[[length(gt)]])$link %>% httr::parse_media(.)} %>% unlist %>% grepl('^next',.) %>% any) {
+n<-0
+while(({httr::headers(gt[[length(gt)]])$link %>% httr::parse_media(.)} %>% unlist %>% grepl('^next',.) %>% any)&n<10) {
   gt[[length(gt)+1]]<-try(httr::GET(nxt),silent = T)
   while(inherits(gt[[length(gt)]],'try-error')) gt[[length(gt)]]<-try(httr::GET(nxt),silent = T)
   con[[length(con)+1]]<-httr::content(gt[[length(gt)]],type='text',encoding = 'UTF-8')
   nxt<-nxtf(gt[[length(gt)]])
+  n<-n+1
 }
 
 writeLines(do.call(c,con),'references.bib')
@@ -42,7 +44,7 @@ sr<-grep('^@',bib)
 br<-grep('(bibtex)|(Citation Key):',bib)
 for(i in br) bib[sr[which(i>sr) %>% max]] %<>% sub('\\{[^,]+',paste0('{',sub('.+: ([^}]+).*','\\1',bib[i])),.)
 writeLines(bib,'references.bib')
-rm(bib,sr,br,i,ini,gt,con,nxt,nxtf)
+rm(bib,sr,br,i,ini,gt,con,nxt,nxtf,n)
 
 
 
