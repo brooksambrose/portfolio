a2f9c311d067956bc6439ea7ed89b5260bb20827
Default user <brooksambrose@gmail.com>
2019-08-28 18:24:15 +0000

    to comittee ahead of defense

--- _build.sh
+++ _build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 git worktree add _book gh-pages || true \
 && rm -rf .RData _book/* _bookdown_files _main*md \
-&& R -e 'bookdown::render_book("index.Rmd", c("bookdown::pdf_book","bookdown::gitbook"))' \
-&& mkdir -p _book/ldaviz/viz && cp -rf ldaviz/viz/*.* _book/ldaviz/viz/
-# && R -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook")'
-# && R -e "rmarkdown::render_site(encoding = 'UTF-8')"
-# && R -e 'bookdown::render_book("index.Rmd", "bookdown::pdf_book",clean=T)'
+&& R -e "rmarkdown::render_site(encoding = 'UTF-8')" \
+&& mkdir -p _book/ldaviz/viz && cp -rf ldaviz/viz/*.* _book/ldaviz/viz/ \
+&& mkdir -p _book/exh && cp -rf exh/* _book/exh/
+
+# && R -e 'bookdown::render_book("index.Rmd", c("bookdown::pdf_book","bookdown::gitbook"))' \
+# && R -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook")' \
+# && R -e 'bookdown::render_book("index.Rmd", "bookdown::pdf_book",clean=T)' \
 
 # command to automatically detect branch and add to edit link
 # $(git branch | grep * | cut -d ' ' -f2)
